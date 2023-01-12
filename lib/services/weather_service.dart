3d03427d5a6afd08bd321f660ad9310e039f2626
Ryan Zander <rjameszander@gmail.com>
2023-01-12 17:26:16 +0700

    cleaned up code, moved classes to separate folders

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:bangkok_weather/api_urls.dart';
+import 'package:bangkok_weather/models/weather.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  /// Returns a Weather model or null
+  Future<Weather?> fetchWeather() async {
+    // Start by checking for location permission
+    final bool hasPermission = await _handlePermission();
+    if (!hasPermission) return null;
+
+    // We have permission, so get the device location
+    final Position position = await Geolocator.getCurrentPosition();
+    final String lat = position.latitude.toString();
+    final String lon = position.longitude.toString();
+
+    // Use the lat and lon to construct the url and make a GET request
+    final String weatherUrl =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$appId&units=metric';
+    final Uri uri = Uri.parse(weatherUrl);
+    final http.Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      // Parse a Weather model from the json for a success response
+      String data = response.body;
+      var json = jsonDecode(data);
+      final Weather weather = Weather.fromJson(json);
+      return weather;
+    } else {
+      log('Request failed with status: ${response.statusCode}.');
+      return null;
+    }
+  }
+
+  Future<bool> _handlePermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return false;
+    }
+
+    // Then check permission and request if currently denied
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return false;
+    }
+
+    // We have permission if we have gotton this far
+    return true;
+  }
+}
--- lib/views/weather_view.dart
+++ lib/views/weather_view.dart
@@ -1,11 +1,6 @@
-import 'dart:convert';
-import 'dart:developer';
-import 'package:bangkok_weather/api_urls.dart';
-import 'package:intl/intl.dart';
-import 'package:geolocator/geolocator.dart';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:bangkok_weather/models/weather.dart';
+import 'package:bangkok_weather/services/weather_service.dart';
 
 class WeatherView extends StatefulWidget {
   const WeatherView({
@@ -116,120 +111,6 @@ class _WeatherViewState extends State<WeatherView> {
         onPressed: loadWeatherData,
         child: const Icon(Icons.refresh),
       ),
-    );
-  }
-}
-
-class WeatherService {
-  /// Returns a Weather model
-  Future<Weather?> fetchWeather() async {
-    // First we need to get the device location
-    final Position position = await _determinePosition();
-    final String lat = position.latitude.toString();
-    final String lon = position.longitude.toString();
-
-    // Use the lat and lon to construct the url and make a GET request
-    final String weatherUrl =
-        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$appId&units=metric';
-    final Uri uri = Uri.parse(weatherUrl);
-    final http.Response response = await http.get(uri);
-
-    if (response.statusCode == 200) {
-      // Parse a Weather model from the json for a success response
-      String data = response.body;
-      var json = jsonDecode(data);
-      final Weather weather = Weather.fromJson(json);
-      return weather;
-    } else {
-      log('Request failed with status: ${response.statusCode}.');
-      return null;
-    }
-  }
-
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
-  }
-}
-
-class Weather {
-  final String name;
-  final String description;
-  final String imageUrl;
-  final int temp;
-  final int feelsLike;
-  final int tempMin;
-  final int tempMax;
-
-  Weather({
-    required this.name,
-    required this.description,
-    required this.imageUrl,
-    required this.temp,
-    required this.feelsLike,
-    required this.tempMin,
-    required this.tempMax,
-  });
-
-  factory Weather.fromJson(Map<String, dynamic> json) {
-    final String name = json['name'] ?? '';
-    final List<dynamic> weather = json['weather'] ?? [];
-    final dynamic currentWeather = weather[0] ?? {};
-    final String description = toBeginningOfSentenceCase(
-            (currentWeather['description'] ?? '').toString()) ??
-        '';
-    final String icon = currentWeather['icon'] ?? '01d';
-    final String imageUrl = 'http://openweathermap.org/img/wn/$icon@2x.png';
-    final dynamic main = json['main'] ?? {};
-    final int temp = (main['temp'] ?? 0.0).round();
-    final int feelsLike = (main['feels_like'] ?? 0.0).round();
-    final int tempMin = (main['temp_min'] ?? 0.0).round();
-    final int tempMax = (main['temp_max'] ?? 0.0).round();
-
-    return Weather(
-      description: description,
-      name: name,
-      imageUrl: imageUrl,
-      temp: temp,
-      feelsLike: feelsLike,
-      tempMin: tempMin,
-      tempMax: tempMax,
     );
   }
 }
